5edae829c650bd5478e32d07d009948b2ae016a6
youngt <youngt.studio.inc@gmail.com>
2020-03-14 18:11:14 +0700

    add Firestore, Firebase Authentication, Firebase Storage

--- android/app/src/main/java/com/orangejuice/MainApplication.java
+++ android/app/src/main/java/com/orangejuice/MainApplication.java
@@ -9,6 +9,8 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import io.invertase.firebase.auth.RNFirebaseAuthPackage;
+import io.invertase.firebase.database.RNFirebaseDatabasePackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -25,6 +27,11 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
+          
+          packages.add(new RNFirebaseAuthPackage());
+          packages.add(new RNFirebaseDatabasePackage());
+          packages.add(new RNFirebaseStoragePackage());
+          
           return packages;
         }
 
